28d9bedd1584598f943d3dbe8aea1afe3a0af378
Cosmsos <cappiah029@st.edu.gh>
2020-08-11 01:50:01 +0000

    create user model

---
+++ server/src/database/models/User.ts
@@ -0,0 +1,31 @@
+import { Schema, Document, model, HookNextFunction } from "mongoose";
+import validator from "validator";
+import { hash } from "bcrypt";
+
+export interface IUser extends Document {
+  email: string;
+  name: string;
+  password: string;
+}
+
+const userSchema = new Schema({
+  name: { type: String, trim: true, required: true },
+  email: {
+    type: String,
+    trim: true,
+    rquired: true,
+    validate(value: string): any {
+      if (!validator.isEmail(value)) throw new Error("not an email");
+    },
+  },
+  password: { type: String, trim: true, required: true },
+});
+
+userSchema.pre<IUser>("save", async function (
+  next: HookNextFunction
+): Promise<void> {
+  if (this.isModified("password")) this.password = await hash(this.password, 8);
+  next();
+});
+
+export default model<IUser>("User", userSchema);
