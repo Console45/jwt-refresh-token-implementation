bcdd683d5d59b9d35d44ff9c416e4a5fb07ad8b5
Cosmsos <cappiah029@st.edu.gh>
2020-08-11 15:18:27 +0000

    added registration route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,21 @@ import express, { Application } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { connection } from "./database/dbConnection";
+import User, { IUser } from "./database/models/User";
 // app config
 const app: Application = express();
 app.use(cors({ origin: "https://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
+
+app.post("/users", async ({ body }, res: any) => {
+  try {
+    const user: IUser = new User(body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 const main = async (): Promise<void> => {
   await connection();
