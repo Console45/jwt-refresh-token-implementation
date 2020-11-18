38ecb7a9d8c82d998f4c865778b6989760c598f5
Cosmsos <cosmosappiah029@gmail.com>
2020-11-18 14:01:50 +0000

    added revoke token class

---
+++ server/src/utils/revokeToken.ts
@@ -0,0 +1,31 @@
+import User from "../database/models/User";
+
+export class RevokeToken {
+  private userId: string;
+  constructor(userId: string) {
+    this.userId = userId;
+  }
+  async refreshToken(): Promise<boolean> {
+    console.log(this.userId);
+    try {
+      await User.findOneAndUpdate(
+        { _id: this.userId },
+        { $inc: { tokenVersion: 1 } }
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+  async resetPasswordToken(): Promise<boolean> {
+    try {
+      await User.findOneAndUpdate(
+        { _id: this.userId },
+        { $inc: { resetPasswordTokenVersion: 1 } }
+      );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
