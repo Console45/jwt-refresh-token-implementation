38feab3f049fae7438b41c942ea61df28b63c73b
Cosmsos <cosmosappiah029@gmail.com>
2020-11-19 09:44:23 +0000

    refactor

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,20 +1,18 @@
 import React, { FC } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { getAccessToken } from "../acessToken";
+import { useUser } from "../hooks/query/useUser";
 
 interface ProtectedRouteProps {
   component: any;
   path: string;
-  user: any;
-  loading: boolean;
 }
 
 export const ProtectedRoute: FC<ProtectedRouteProps> = ({
   component: Component,
   path,
-  loading,
-  user,
 }) => {
+  const { user, loading } = useUser();
   const accessToken = getAccessToken();
   if (loading) return <p>loading...</p>;
   return accessToken && user ? (
--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Header } from "../components/Header";
-import { useUser } from "../hooks/useUser";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Profile } from "../pages/Profile";
@@ -9,7 +8,6 @@ import { Register } from "../pages/Register";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export const Routes: FC = () => {
-  const { user, loading } = useUser();
   return (
     <BrowserRouter>
       <div>
@@ -24,12 +22,7 @@ export const Routes: FC = () => {
           <Route exact path="/login">
             <Login />
           </Route>
-          <ProtectedRoute
-            path="/user"
-            user={user}
-            loading={loading}
-            component={Profile}
-          />
+          <ProtectedRoute path="/user" component={Profile} />
         </Switch>
       </div>
     </BrowserRouter>
