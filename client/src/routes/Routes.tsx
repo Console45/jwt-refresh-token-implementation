a8d02fd0b57617695d4c74e629ff2784c2ab6062
Cosmsos <cosmosappiah029@gmail.com>
2020-11-19 03:19:30 +0000

    fetch user on render

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Header } from "../components/Header";
+import { useUser } from "../hooks/useUser";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Profile } from "../pages/Profile";
@@ -8,6 +9,7 @@ import { Register } from "../pages/Register";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export const Routes: FC = () => {
+  const { user, loading } = useUser();
   return (
     <BrowserRouter>
       <div>
@@ -22,7 +24,12 @@ export const Routes: FC = () => {
           <Route exact path="/login">
             <Login />
           </Route>
-          <ProtectedRoute path="/user" component={Profile} />
+          <ProtectedRoute
+            path="/user"
+            user={user}
+            loading={loading}
+            component={Profile}
+          />
         </Switch>
       </div>
     </BrowserRouter>
